4542651da977c06356e1e61560a2fc31146f9609
pablopierre92 <pablo_chips@hotmail.com>
2024-04-24 11:45:03 +0100

    Configurando botões adicionar tarefa e deletar tarefas completas

--- ListaDeTarefas/ListaDeTarefas/Controllers/HomeController.cs
+++ ListaDeTarefas/ListaDeTarefas/Controllers/HomeController.cs
@@ -17,14 +17,14 @@ namespace ListaDeTarefas.Controllers
 		public IActionResult Index(string id)
 		{
 			var filtros = new Filtros(id);
-			
+
 			ViewBag.Filtros = filtros;
 			ViewBag.Categorias = _context.Categorias.ToList();
 			ViewBag.Status = _context.Statuses.ToList();
 			ViewBag.VencimentoValores = Filtros.VencimentoValoresFiltro;
 
 			IQueryable<Tarefa> consulta = _context.Tarefas.Include(c => c.Categoria).Include(s => s.Status); //ir na tabela de tarefas e buscar todas as categorias e status
-			
+
 			if (filtros.TemCategoria)
 			{
 				consulta = consulta.Where(t => t.CategoriaId == filtros.CategoriaId);
@@ -61,6 +61,66 @@ namespace ListaDeTarefas.Controllers
 			return View(tarefas);
 		}
 
+		public IActionResult Adicionar()
+		{
+			ViewBag.Categorias = _context.Categorias.ToList();
+			ViewBag.Status = _context.Statuses.ToList();
 
+			var tarefa = new Tarefa { StatusId = "aberto" };
+
+			return View(tarefa);
+		}
+
+		[HttpPost]
+		public IActionResult Adicionar(Tarefa tarefa)
+		{
+			if (ModelState.IsValid)
+			{
+				_context.Tarefas.Add(tarefa);
+				_context.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			else
+			{
+				ViewBag.Categorias = _context.Categorias.ToList();
+				ViewBag.Status = _context.Statuses.ToList();
+			}
+
+			return View(tarefa);
+		}
+
+		[HttpPost]
+		public IActionResult Filtrar(string[] filtro)
+		{
+			string id = string.Join('-', filtro);
+			return RedirectToAction("Index", new { ID = id });
+		}
+
+		[HttpPost]
+		public IActionResult MarcarCompleto([FromRoute] string id, Tarefa tarefaSelecionada)
+		{
+			tarefaSelecionada = _context.Tarefas.Find(tarefaSelecionada.Id);
+
+			if (tarefaSelecionada != null)
+			{
+				tarefaSelecionada.StatusId = "completo";
+				_context.SaveChanges();
+			}
+			return RedirectToAction("Index", new { ID = id });
+		}
+
+		[HttpPost]
+		public IActionResult DeletarCompletos(string id)
+		{
+			var paraDeletar = _context.Tarefas.Where(s => s.StatusId == "completo").ToList();
+
+			foreach(var tarefa in paraDeletar)
+			{
+				_context.Tarefas.Remove(tarefa);
+			}
+			_context.SaveChanges();
+
+			return RedirectToAction("Index", new {ID = id});
+		}
 	}
 }
