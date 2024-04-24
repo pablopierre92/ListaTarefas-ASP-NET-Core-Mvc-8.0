b97ce4c863a547c9d8ce6e3d1d31afdf22aaff2b
pablopierre92 <pablo_chips@hotmail.com>
2024-04-24 16:22:23 +0100

    Adicionado botões editar e excluir categorias

---
+++ ListaDeTarefas/ListaDeTarefas/Controllers/CategoriasController.cs
@@ -0,0 +1,108 @@
+﻿using ListaDeTarefas.Data;
+using ListaDeTarefas.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ListaDeTarefas.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        private readonly AppDbContext _context;
+        public CategoriasController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Categoria> categoria = _context.Categorias;
+            return View(categoria);
+        }
+
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+
+        public IActionResult Editar(string? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            Categoria categoria = _context.Categorias.FirstOrDefault(x => x.CategoriaId == id);
+
+            if(categoria == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoria);
+        }
+
+
+		public IActionResult Excluir(string? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
+
+			Categoria categoria = _context.Categorias.FirstOrDefault(x => x.CategoriaId == id);
+
+			if (categoria == null)
+			{
+				return NotFound();
+			}
+
+			return View(categoria);
+		}
+
+		[HttpPost]
+        public IActionResult Cadastrar(Categoria categoria)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Categorias.Add(categoria);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public IActionResult Editar(Categoria categoria)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Categorias.Update(categoria);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(categoria);
+        }
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+
+		public IActionResult Excluir(Categoria categoria)
+		{
+			if (categoria == null)
+            {
+                return NotFound();
+            }
+
+            _context.Categorias.Remove(categoria);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+
+		}
+	}
+}
